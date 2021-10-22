20bbd368ad92434e290703a6b25c3295a2c66838
Nico Franke <nico.franke01@gmail.com>
2021-10-22 17:56:47 +0200

    :tada: Init spring project

---
+++ src/main/java/com/kbe/unicorn/UnicornApplication.java
@@ -0,0 +1,13 @@
+package com.kbe.unicorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicornApplication.class, args);
+	}
+
+}

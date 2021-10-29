06ea5193259c38d8410ca42eb78629313b493b14
Nico Franke <nico.franke01@gmail.com>
2021-10-29 16:41:52 +0200

    :sparkles: Add get one song route

---
+++ src/main/java/com/kbe/unicorn/song/SongService.java
@@ -0,0 +1,16 @@
+package com.kbe.unicorn.song;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class SongService {
+    @Autowired
+    SongRepository repo;
+
+    public SongEntity findById(long id) {
+        return repo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+}

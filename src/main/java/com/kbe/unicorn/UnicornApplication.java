d64806bf94761eab0ba7dc11df1564c093ad096e
Thorben0 <thorben.stumpe@gmail.com>
2021-10-31 17:44:43 +0100

    :art: Organize imports

--- src/main/java/com/kbe/unicorn/UnicornApplication.java
+++ src/main/java/com/kbe/unicorn/UnicornApplication.java
@@ -1,17 +1,9 @@
 package com.kbe.unicorn;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kbe.unicorn.song.SongEntity;
-import com.kbe.unicorn.song.SongRepository;
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-
-import java.nio.file.Paths;
-import java.util.Arrays;
 
 @SpringBootApplication
 public class UnicornApplication {
--- src/main/java/com/kbe/unicorn/song/SongEntity.java
+++ src/main/java/com/kbe/unicorn/song/SongEntity.java
@@ -1,11 +1,11 @@
 package com.kbe.unicorn.song;
 
-import lombok.Data;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import lombok.Data;
 
 @Data
 @Entity

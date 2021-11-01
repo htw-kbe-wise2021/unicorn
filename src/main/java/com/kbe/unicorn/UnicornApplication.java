244151ed7c32cd445b10bb559427827d2562cdf6
Nico Franke <nico.franke01@gmail.com>
2021-11-01 08:26:16 +0100

    :bug: Fix swagger on https

--- src/main/java/com/kbe/unicorn/UnicornApplication.java
+++ src/main/java/com/kbe/unicorn/UnicornApplication.java
@@ -1,10 +1,15 @@
 package com.kbe.unicorn;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@OpenAPIDefinition(info = @Info(title = "Unicorn API", version = "1.0"),
+		servers = @Server(url = "/songsservlet-unicorn", description = "Magical API"))
 @SpringBootApplication
 public class UnicornApplication {
 
